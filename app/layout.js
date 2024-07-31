da4a897568cacf7b69b73ea6e30b0acafcc36798
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2024-07-30 20:20:00 -0600

    sprint 1 update

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <body className={inter.className}>{children}</body> */}
-      <AuthContextProvider>{children}</AuthContextProvider>
+      <body className={inter.className}>
+        <AuthContextProvider>{children}</AuthContextProvider>
+      </body>
     </html>
   );
 }
